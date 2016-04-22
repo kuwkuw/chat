cae17947c5944e22e86035c7aa60a7f2d36131f4
kuwkuw <kuw-kuw@mail.ru>
2016-04-22 11:21:32 +0300

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //context: __dirname + "/app",
+    entry: "./frontend/app.js",
+    output: {
+        path: __dirname + "/public/javascripts",
+        filename: "bundle.js"
+    }
+};
